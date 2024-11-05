c22d19391ac873ab677327aec091454d1bd730a0
Dana <dana.joukhadar3@gmail.com>
2024-11-06 01:29:53 +0300

    Apologizing

--- dana_and_lujain_git.py
+++ dana_and_lujain_git.py
@@ -1,6 +1,5 @@
-i=0
-print("This is Dana Typing")
-
 i=1
 print("Lujain removed semicolon (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)")
-print("Lujain will push this code for the first time")
+print("it's almost 2 in the morning ")
+print("Dana is sorry for the hbd :) ")
+print(i)
